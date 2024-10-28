a6e65c67b3ffb0b0fc7fe0ae5170a9116f9218d7
Mingzhe Du <dumingzhe@126.com>
2024-10-28 12:33:37 +0000

    Update new agent

--- agent.py
+++ agent.py
@@ -1,24 +1,18 @@
 # coding: utf-8
 
 # Author: Du Mingzhe (mingzhe@nus.edu.sg)
-# Date: 2024-10-18
-
-import os
-os.environ['HF_HUB_CACHE'] = '/mnt/data'
-os.environ['HF_HUB_ENABLE_HF_TRANSFER'] = '1'
+# Date: 2024-10-28
 
 from transformers import pipeline
 from transformers import BitsAndBytesConfig
 
-class AbstractAgent:
+class Agent:
     def __init__(self, model_name):
         self.model_name = model_name
         self.pipe = pipeline(
             "text-generation",
             model=self.model_name,
-            model_kwargs={
-                "quantization_config": BitsAndBytesConfig(load_in_8bit=True)
-            },
+            model_kwargs={"quantization_config": BitsAndBytesConfig(load_in_8bit=True)},
             device_map="auto",
         )
         
@@ -37,9 +31,7 @@ if __name__ == "__main__":
         {"role": "user", "content": "Hello. Write me a poem."},
     ]
     
-    agent = AbstractAgent("meta-llama/Llama-3.1-8B-Instruct")
+    agent = Agent("meta-llama/Llama-3.1-8B-Instruct")
     
     response = agent.generate(messages, max_new_tokens=256, temperature=0.0)
     print(response)
-
-
