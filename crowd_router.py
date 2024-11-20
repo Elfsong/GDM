88dbc2064526d55b59bf2f3c0e7976aba359d984
Mingzhe Du <dumingzhe@126.com>
2024-11-20 16:30:02 +0000

    Update Router

--- crowd_router.py
+++ crowd_router.py
@@ -3,6 +3,7 @@
 # Author: Du Mingzhe (mingzhe@nus.edu.sg)
 # Date: 2024-11-20
 
+import torch
 import crowd_agent
 
 class Router:
@@ -15,7 +16,14 @@ class Router:
         return bias_category
         
     def route(self, query, top_k=3):
-        pass
+        model_probabilities = []
+        with torch.no_grad():
+            for index, model_name in enumerate(self.model_list):
+                inputs = self.router_tokenizer(query + f'model_{index}', return_tensors="pt")
+                outputs = self.router_model(**inputs, labels=inputs["input_ids"])
+                loss = outputs.loss
+                model_probabilities.append((model_name, torch.exp(-loss).item()))
+        return model_probabilities
     
 if __name__ == "__main__":
     agent_manager = crowd_agent.AgentManager()
